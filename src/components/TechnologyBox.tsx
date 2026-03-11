038ba12f879a1513646144bade6d496a1d48a7b4
Blue Jessen <canealisblue@gmail.com>
2026-03-10 22:08:42 -0400

    Continue updating technology import

--- src/components/TechnologyBox.tsx
+++ src/components/TechnologyBox.tsx
@@ -1,24 +1,22 @@
+import styles from './TechnologyBox.module.css'
 interface technologyProps {
 name: string,
-experience: Array<string>, 
+experience: Array<{tag: string, desc: string}>, 
 icon: string,
 }
 
 const TechnologyBox = ({name, experience, icon}: technologyProps) => {
     return(
-        <div className="technology-box">
-            <div className="technology-header"> 
+        <div className={styles.technologyBox}>
+            <div className={styles.technologyHeader}> 
                 <img src={icon} alt={name}/>
                 <h3>
                     {name}
                 </h3>
             </div>
-           
-            { experience.map(() => {
+            { experience.map((item) => {
                 return(
-                    <div>
-                        <a href={`#${experience}`}>{experience}</a>
-                    </div>
+                        <a key={item.tag} href={`#${item.tag}`}>{item.desc}</a>
                     )
                 }
             )
--- src/data/technologies.tsx
+++ src/data/technologies.tsx
@@ -1,11 +1,11 @@
-const technologies = [{"name": "React", "icon": "", "experience": ["GSA-bah"]},
-{"name": "Vue", "icon": "", "experience": ["tag"]},
-{"name": "Next.js", "icon": "", "experience": ["tag"]},
-{"name": "CSS", "icon": "", "experience": ["tag"]},
-{"name": "Typescript", "icon": "", "experience": ["tag"]},
-{"name": "JavaScript", "icon": "", "experience": ["tag"]},
-{"name": "HTML", "icon": "", "experience": ["tag"]},
-{"name": "Python", "icon": "", "experience": ["tag"]}
+const technologies = [{"name": "React", "icon": "", "experience": [{tag:"GSA-bah", desc: "Product Marketing and Self Service Portal at GSA"}]},
+{"name": "Vue", "icon": "", "experience": [{tag:"tag", desc: "desc"}]},
+{"name": "Next.js", "icon": "", "experience": [{tag:"tag", desc: "desc"}]},
+{"name": "CSS", "icon": "", "experience": [{tag:"tag", desc: "desc"}]},
+{"name": "Typescript", "icon": "", "experience": [{tag:"tag", desc: "desc"}]},
+{"name": "JavaScript", "icon": "", "experience": [{tag:"tag", desc: "desc"}]},
+{"name": "HTML", "icon": "", "experience": [{tag:"tag", desc: "desc"}]},
+{"name": "Python", "icon": "", "experience": [{tag:"tag", desc: "desc"}]}
 ];
 
 export default technologies;
