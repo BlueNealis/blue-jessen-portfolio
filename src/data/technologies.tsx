bf6f90cabc0d15e22b6d1413a28d25e8eb23f502
Blue Jessen <canealisblue@gmail.com>
2026-03-14 22:49:15 -0400

    Add rest of icons

--- src/data/technologies.tsx
+++ src/data/technologies.tsx
@@ -1,6 +1,6 @@
 const technologies = [{"name": "React", "icon": "src/assets/atom.png", "experience": [{tag:"GSA-bah", desc: "GSA - Booz Allen Hamilton"}, {tag:"turing", desc: "Turing School"}]},
-{"name": "Vue", "icon": "/assets/", "experience": [{tag:"tag", desc: "desc"}]},
-{"name": "Next.js", "icon": "", "experience": [{tag:"tag", desc: "desc"}]},
+{"name": "Vue", "icon": "src/assets/vue.png", "experience": [{tag:"tag", desc: "desc"}]},
+{"name": "Next.js", "icon": "src/assets/next.png", "experience": [{tag:"tag", desc: "desc"}]},
 {"name": "CSS", "icon": "src/assets/css-3.png", "experience": [{tag:"tag", desc: "desc"}]},
 {"name": "Sass", "icon": "src/assets/sass.png", "experience": [{tag:"tag", desc: "desc"}]},
 {"name": "Typescript", "icon": "src/assets/typescript.png", "experience": [{tag:"tag", desc: "desc"}]},
