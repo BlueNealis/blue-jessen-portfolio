13204cdfa7fb04e4f53d681af55acf573aa833a7
Blue Jessen <canealisblue@gmail.com>
2026-05-14 23:07:06 -0400

    Add tags

--- src/data/technologies.tsx
+++ src/data/technologies.tsx
@@ -1,12 +1,60 @@
-const technologies = [{"name": "React", "icon": "src/assets/atom.png", "experience": [{tag:"GSA-bah", desc: "GSA - Booz Allen Hamilton"}, {tag:"turing", desc: "Turing School"}]},
-{"name": "Vue", "icon": "src/assets/vue.png", "experience": [{tag:"tag", desc: "desc"}]},
-{"name": "Next.js", "icon": "src/assets/next.png", "experience": [{tag:"tag", desc: "desc"}]},
-{"name": "CSS", "icon": "src/assets/css-3.png", "experience": [{tag:"tag", desc: "desc"}]},
-{"name": "Sass", "icon": "src/assets/sass.png", "experience": [{tag:"tag", desc: "desc"}]},
-{"name": "Typescript", "icon": "src/assets/typescript.png", "experience": [{tag:"tag", desc: "desc"}]},
-{"name": "JavaScript", "icon": "src/assets/js.png", "experience": [{tag:"tag", desc: "desc"}]},
-{"name": "HTML", "icon": "src/assets/html-5.png", "experience": [{tag:"tag", desc: "desc"}]},
-{"name": "Python", "icon": "src/assets/python.png", "experience": [{tag:"tag", desc: "desc"}]}
+const technologies = [
+    {
+        "name": "React", 
+        "icon": "src/assets/atom.png", 
+        "experience": [
+            {tag:"GSA-bah", 
+                desc: "GSA - Booz Allen Hamilton"}, 
+            {tag:"turing", 
+                desc: "Turing School"}]},
+    {
+        "name": "Vue", 
+        "icon": "src/assets/vue.png", 
+        "experience": [
+            {tag:"DOT-bah", desc: "DOT - Booz Allen Hamilton"}]},
+    {
+        "name": "Next.js", 
+        "icon": "src/assets/next.png", 
+        "experience": [
+            {tag:"GSA-bah", desc: "GSA - Booz Allen Hamilton"}]},
+    {
+        "name": "CSS", 
+        "icon": "src/assets/css-3.png", 
+        "experience": [
+            {tag:"GSA-bah", desc: "GSA - Booz Allen Hamilton"},
+            {tag:"DOT-bah", desc: "DOT - Booz Allen Hamilton"},
+            {tag:"turing", desc: "Turing School"}]},
+    {
+        "name": "Sass", 
+        "icon": "src/assets/sass.png", 
+        "experience": [
+            {tag:"GSA-bah", desc: "GSA - Booz Allen Hamilton"},
+            {tag:"turing", desc: "Turing School"}]},
+    {
+        "name": "Typescript", 
+        "icon": "src/assets/typescript.png", 
+        "experience": [
+            {tag:"GSA-bah", desc: "GSA - Booz Allen Hamilton"},
+            {tag:"turing", desc: "Turing School"}]},
+    {
+        "name": "JavaScript", 
+        "icon": "src/assets/js.png", 
+        "experience": [
+            {tag:"GSA-bah", desc: "GSA - Booz Allen Hamilton"},
+            {tag:"DOT-bah", desc: "DOT - Booz Allen Hamilton"},
+            {tag:"turing", desc: "Turing School"}]},
+    {
+        "name": "HTML", 
+        "icon": "src/assets/html-5.png", 
+        "experience": [
+            {tag:"GSA-bah", desc: "GSA - Booz Allen Hamilton"},
+            {tag:"DOT-bah", desc: "DOT - Booz Allen Hamilton"},
+            {tag:"turing", desc: "Turing School"}]},
+    {
+        "name": "Python", 
+        "icon": "src/assets/python.png", 
+        "experience": [
+            {tag:"tag", desc: "desc"}]}
 ];
 
 export default technologies;
